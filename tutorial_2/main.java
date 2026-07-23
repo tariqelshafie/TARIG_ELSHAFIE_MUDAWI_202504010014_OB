53c0f17eaa82921dfb761d348d6985bb381f87d6
tariqelshafie <tariq.elshafie@gmail.com>
2026-07-23 04:42:54 +0000

    complete_tutorial_2

---
+++ tutorial_2/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        //coding starts here;
+
+        Student s1 = new Student("Tariq",23,4.0);
+        s1.displayInfo();
+        s1.study();
+        s1.takeExam();
+    }
+}
